8ebf1539171ef92c269a260985e5055bfad17b07
Ava-creates <avanitiwari2002@gmail.com>
2022-02-21 23:24:21 +0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+function getScrollPercent()
+{
+  let scrollRange = document.body.offsetHeight - window.innerHeight;
+  return window.scrollY / scrollRange;
+}
+
 window.addEventListener('scroll', function(e){
-    
+    console.log(window.scrollY, getScrollPercent());
     let percent = getScrollPercent();
-    document.body.style.backgroundColor = 'rgb(0, 0,' + percent* 255+')';
+    document.body.style.backgroundColor = 'rgb(150, 50,' + (percent)* 200+')';
   });
+
+
+  function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
